0b596ad45867739ad66a2cad87f1b6fc7db9dfdc
gabriel.lrcosta <gabriel.lrcosta@sp.senac.br>
2023-06-16 15:39:11 -0300

    feat: creating header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
+
+import { Header } from "@/components/header/header"
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        
-      </div>
+    <main >
+      <Header />
+      
     </main>
   )
 }
---
+++ src/components/header/header.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { styled } from "styled-components"
+import { Logo } from "../svg-components/logo"
+
+const HeaderStyled = styled.header`
+    justify-content: space-between;
+    background: var(--primary-color);
+    height: 177px;
+`
+    const TransactionButton = styled.button`
+        background-color: var(--secondary-color);
+        color: var(--gray-text);
+        border: none;
+        border-radius: 8px;
+        width: 245px;
+        height: 53px;
+        font-weight: bold;
+        cursor: pointer;
+    `
+
+export function Header(){
+    return(
+        
+        <HeaderStyled className="container">
+            <Logo></Logo>
+            <TransactionButton onClick={() =>{console.log('click')}}>NOVA TRANSAÇÃO</TransactionButton>
+        </HeaderStyled>
+        
+    )
+}
